84f7a35a087494cc020b7932903f2c9da8b7efde
Rus <de.mr44@gmail.com>
2022-01-19 22:53:21 +0500

    Initial Commit

---
+++ Patterns-Flyweight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Patterns-Flyweight
+//
+//  Created by Ruslan on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
